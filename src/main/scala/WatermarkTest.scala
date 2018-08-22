82c650a579619dab57c97bd7afa42e04098e4c7a
su <su>
2018-08-22 16:07:58 +0800

    test

--- src/main/scala/WatermarkTest.scala
+++ src/main/scala/WatermarkTest.scala
@@ -31,9 +31,11 @@ object WatermarkTest {
       val arr = f.split("\\W+")
       val code = arr(0)
       val time = arr(1).toLong
+      //在这里对数据进行抽象，讲数据进行tup处理成返回的元组
       (code, time)
     })
-
+    //new AssignerWithPeriodicWatermarks[(String,Long)中有抽取timestamp和生成watermark2个方法，在执行时，
+    // 它是先抽取timestamp，后生成watermark，因此我们在这里print的watermark时间，其实是上一条的watermark时间，我们到数据输出时再解释
     val watermark = inputMap.assignTimestampsAndWatermarks(new AssignerWithPeriodicWatermarks[(String, Long)] {
 
       var currentMaxTimestamp = 0L
@@ -42,15 +44,18 @@ object WatermarkTest {
       var a: Watermark = null
 
       val format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
-
+      //然后才执行这个方法
       override def getCurrentWatermark: Watermark = {
         a = new Watermark(currentMaxTimestamp - maxOutOfOrderness)
         a
       }
 
+      //先执行这个方法
+      //提取timestamp生成watermark，并且打印
       override def extractTimestamp(t: (String,Long), l: Long): Long = {
         val timestamp = t._2
         currentMaxTimestamp = Math.max(timestamp, currentMaxTimestamp)
+        //并打印（code，time，格式化的time，currentMaxTimestamp，currentMaxTimestamp的格式化时间，watermark时间（）
         println("timestamp:" + t._1 +","+ t._2 + "|" +format.format(t._2) +","+  currentMaxTimestamp + "|"+ format.format(currentMaxTimestamp) + ","+ a.toString)
         timestamp
       }
