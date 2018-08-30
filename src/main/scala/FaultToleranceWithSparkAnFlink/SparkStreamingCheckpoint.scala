55081cc9b9eccb52ee8ee48728ad67805fe3228e
su <su>
2018-08-30 11:26:13 +0800

    init

--- src/main/scala/FaultToleranceWithSparkAnFlink/SparkStreamingCheckpoint.scala
+++ src/main/scala/FaultToleranceWithSparkAnFlink/SparkStreamingCheckpoint.scala
@@ -1,14 +1,19 @@
 package FaultToleranceWithSparkAnFlink
 
-import org.apache.flink.api.java.utils.ParameterTool
+
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.dstream.{DStream, ReceiverInputDStream}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object SparkStreamingCheckpoint {
   def ssc: StreamingContext={
-    val sparkConf = new SparkConf().setAppName("SparkStreamingCheckpoint").setMaster("local[2]").set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    val sparkConf = new SparkConf().
+      setAppName("SparkStreamingCheckpoint")
+      .setMaster("local[2]")
+      .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      .set("spark.driver.allowMultipleContexts","true")
     val ssc: StreamingContext = new StreamingContext(sparkConf,Seconds(2))   // new context
+    ssc.checkpoint("E:\\log")   // set checkpoint directory
     ssc
   }
   def main(args: Array[String]): Unit = {
@@ -18,9 +23,7 @@ object SparkStreamingCheckpoint {
     }
     val hostName = args(0)
     val port = args(1).toInt
-
     val lines: ReceiverInputDStream[String] = ssc.socketTextStream(hostName,port)
-    ssc.checkpoint("E:\\log")   // set checkpoint directory
     val words: DStream[String] = lines.flatMap(_.split(" "))
     val wordAndOne: DStream[(String, Int)] = words.map((_, 1))
     val wordCount: DStream[(String, Int)] = wordAndOne.reduceByKey(_ + _)
