70ae6f6c611d9fa5385f2f4cb94eddda07ae5dfe
su <su>
2018-08-30 11:34:35 +0800

    init

--- src/main/scala/FaultToleranceWithSparkAnFlink/SparkStreamingCheckpoint.scala
+++ src/main/scala/FaultToleranceWithSparkAnFlink/SparkStreamingCheckpoint.scala
@@ -6,16 +6,7 @@ import org.apache.spark.streaming.dstream.{DStream, ReceiverInputDStream}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object SparkStreamingCheckpoint {
-  def ssc: StreamingContext={
-    val sparkConf = new SparkConf().
-      setAppName("SparkStreamingCheckpoint")
-      .setMaster("local[2]")
-      .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-      .set("spark.driver.allowMultipleContexts","true")
-    val ssc: StreamingContext = new StreamingContext(sparkConf,Seconds(2))   // new context
-    ssc.checkpoint("E:\\log")   // set checkpoint directory
-    ssc
-  }
+
   def main(args: Array[String]): Unit = {
     if (args.length != 2) {
       System.err.println("USAGE:\nSocketWatermarkTest <hostname> <port>")
@@ -23,6 +14,14 @@ object SparkStreamingCheckpoint {
     }
     val hostName = args(0)
     val port = args(1).toInt
+    val sparkConf = new SparkConf().
+      setAppName("SparkStreamingCheckpoint")
+      .setMaster("local[2]")
+      .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      .set("spark.driver.allowMultipleContexts","true")
+    val ssc: StreamingContext = new StreamingContext(sparkConf,Seconds(2))   // new context
+    ssc.checkpoint("E:\\log")   // set checkpoint directory
+    
     val lines: ReceiverInputDStream[String] = ssc.socketTextStream(hostName,port)
     val words: DStream[String] = lines.flatMap(_.split(" "))
     val wordAndOne: DStream[(String, Int)] = words.map((_, 1))
